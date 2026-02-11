d2314f0790255226b8f1c6c0a9a6ccc85eb9f2dc
Nouri3406 <nouri.foudil-bey@epitech.eu>
2026-02-11 03:47:16 +0100

    correction mise en ligne

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Portfolio-React">
     <App />
   </BrowserRouter>
 )
